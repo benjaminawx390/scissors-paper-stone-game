efa0fac4d6e6d151eea72757f77ac87888e6a24c
benjaminawx390 <benawx390@gmail.com>
2024-11-24 22:59:26 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//Scissor paper stone game project
+
+
+//Computer Functions
+
+function computerChoice () {
+    const choices=["scissors", "paper" , "stone"];
+    const random=Math.floor(Math.random() * choices.length);
+    return choices[random]
+}
+
+
+//determining the winner
+
+function determineWinner(user , computer){
+    if(user===computer){
+        console.log(
+            "This round is a tie!"
+        );
+    }else if (user ==="scissors" && computer==="paper" || user ==="paper"  && computer ==="stone" || user ==="stone" && computer==="scissors"){
+        console.log(
+            "You win !"
+        );
+    }
+    else{
+        console.log(
+            "You lose!"
+        );
+    }
+}
+
+
+//function for main game
+function mainGame 
