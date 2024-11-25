836bf9d3dfcfa3c300fee51e2e89a792f429a184
benjaminawx390 <benawx390@gmail.com>
2024-11-25 19:47:29 +0800

    added the mainGame() function

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function computerChoice () {
 }
 
 
-//determining the winner
+//determining the winner with logical condition
 
 function determineWinner(user , computer){
     if(user===computer){
@@ -29,3 +29,25 @@ function determineWinner(user , computer){
     }
 }
 
+//The actual game function
+
+function mainGame(){
+    let user = prompt("Scissors paper or stone?");
+    user = user.toLowerCase(); //All inputs to be changed to small caps
+
+    if (user !== "scissors" && user !== "paper" && user !== "stone"){
+        console.log("Please enter a valid input again between scissor, paper and stone.");
+    }
+    const computer = computerChoice(); 
+
+    //Showing choices
+    console.log(`You chose ${user}`);
+    console.log(`The computer chose ${computer}`);
+
+    //Using the determine winner function
+
+    const result = determineWinner(user,computer);
+    console.log(result);
+}
+
+mainGame();
